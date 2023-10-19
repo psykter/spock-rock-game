fc2063e1e29a191fede379092dca1cac01c24c53
Rick Diaz <richard.david.diaz@gmail.com>
2023-10-19 11:54:22 -0500

    Creating game rock,paper,sissors,lizzard,spock. html and min js,css

---
+++ js/confetti.js
@@ -0,0 +1,161 @@
+// Confetti.js - downloaded from https://www.cssscript.com/confetti-falling-animation/
+
+var confetti = {
+  maxCount: 150, //set max confetti count
+  speed: 2, //set the particle animation speed
+  frameInterval: 15, //the confetti animation frame interval in milliseconds
+  alpha: 1.0, //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
+  gradient: false, //whether to use gradients for the confetti particles
+  start: null, //call to start confetti animation (with optional timeout in milliseconds, and optional min and max random confetti count)
+  stop: null, //call to stop adding confetti
+  toggle: null, //call to start or stop the confetti animation depending on whether it's already running
+
+};
+
+confetti.start = startConfetti;
+confetti.stop = stopConfetti;
+confetti.toggle = toggleConfetti;
+
+var supportsAnimationFrame =
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+var colors = [
+  "rgba(30,144,255,",
+  "rgba(107,142,35,",
+  "rgba(255,215,0,",
+  "rgba(255,192,203,",
+  "rgba(106,90,205,",
+  "rgba(173,216,230,",
+  "rgba(238,130,238,",
+  "rgba(152,251,152,",
+  "rgba(70,130,180,",
+  "rgba(244,164,96,",
+  "rgba(210,105,30,",
+  "rgba(220,20,60,",
+];
+var streamingConfetti = false;
+var animationTimer = null;
+var pause = false;
+var lastFrameTime = Date.now();
+var particles = [];
+var waveAngle = 0;
+var context = null;
+
+function resetParticle(particle, width, height) {
+  particle.color =
+    colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+  particle.color2 =
+    colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+  particle.x = Math.random() * width;
+  particle.y = Math.random() * height - height;
+  particle.diameter = Math.random() * 10 + 5;
+  particle.tilt = Math.random() * 10 - 10;
+  particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
+  particle.tiltAngle = Math.random() * Math.PI;
+  return particle;
+}
+
+
+
+function runAnimation() {
+  if (pause) return;
+  else if (particles.length === 0) {
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    animationTimer = null;
+  } else {
+    var now = Date.now();
+    var delta = now - lastFrameTime;
+    if (!supportsAnimationFrame || delta > confetti.frameInterval) {
+      context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      updateParticles();
+      drawParticles(context);
+      lastFrameTime = now - (delta % confetti.frameInterval);
+    }
+    animationTimer = requestAnimationFrame(runAnimation);
+  }
+}
+
+function startConfetti(timeout, min, max) {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  window.requestAnimationFrame = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (callback) {
+        return window.setTimeout(callback, confetti.frameInterval);
+      }
+    );
+  })();
+  var canvas = document.getElementById("confetti-canvas");
+  if (canvas === null) {
+    canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "confetti-canvas");
+    canvas.setAttribute(
+      "style",
+      "display:block;z-index:999999;pointer-events:none;position:fixed;top:0"
+    );
+    document.body.prepend(canvas);
+    canvas.width = width;
+    canvas.height = height;
+    window.addEventListener(
+      "resize",
+      function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      },
+      true
+    );
+    context = canvas.getContext("2d");
+  } else if (context === null) context = canvas.getContext("2d");
+  var count = confetti.maxCount;
+  if (min) {
+    if (max) {
+      if (min == max) count = particles.length + max;
+      else {
+        if (min > max) {
+          var temp = min;
+          min = max;
+          max = temp;
+        }
+        count = particles.length + ((Math.random() * (max - min) + min) | 0);
+      }
+    } else count = particles.length + min;
+  } else if (max) count = particles.length + max;
+  while (particles.length < count)
+    particles.push(resetParticle({}, width, height));
+  streamingConfetti = true;
+  pause = false;
+  runAnimation();
+  if (timeout) {
+    window.setTimeout(stopConfetti, timeout);
+  }
+}
+
+function stopConfetti() {
+  streamingConfetti = false;
+}
+
+function removeConfetti() {
+  stop();
+  pause = false;
+  particles = [];
+}
+
+function toggleConfetti() {
+  if (streamingConfetti) stopConfetti();
+  else startConfetti();
+}
+
+function isConfettiRunning() {
+  return streamingConfetti;
+}
+
+
+
---
+++ js/script.js
@@ -0,0 +1,86 @@
+import { startConfetti, stopConfetti, removeConfetti } from './confetti.js';
+
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+const allGameIcons = document.querySelectorAll('.far');
+const resultText = document.getElementById('resultText');
+
+const choices = {
+  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
+  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
+  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
+  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
+  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+};
+
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = '';
+
+
+
+
+
+// Random computer choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.2) {
+    computerChoice = 'rock';
+  } else if (computerChoiceNumber <= 0.4) {
+    computerChoice = 'paper';
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = 'scissors';
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
+  }
+}
+
+
+
+// Check result, increase scores, update resultText
+function updateScore(playerChoice) {
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie.";
+  } else {
+    const choice = choices[playerChoice];
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+      startConfetti();
+      resultText.textContent = 'You Won!';
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
+    } else {
+      resultText.textContent = 'You Lost!';
+      computerScoreNumber++;
+      computerScoreEl.textContent = computerScoreNumber;
+    }
+  }
+}
+
+// Call functions to process turn
+function checkResult(playerChoice) {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice);
+}
+
+
+
+
